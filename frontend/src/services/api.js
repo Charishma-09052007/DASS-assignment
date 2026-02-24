3697dcc8c4c00d135f9beb5942705046b10c49cc
Charishma-09052007 <charishmagaddampally@gmail.com>
2026-02-24 11:39:40 +0530

    Use env API URL in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
+console.log("🌍 API BASE URL:", API_BASE_URL);
+
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
